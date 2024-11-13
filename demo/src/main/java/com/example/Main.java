f7fcd01037d2c0ed1c5444e50167297c7c613961
= <ahmedamr_2003@yahoo.com>
2024-11-13 18:07:49 +0200

    extra comment

--- demo/src/main/java/com/example/Main.java
+++ demo/src/main/java/com/example/Main.java
@@ -8,6 +8,7 @@ public class Main {
     public static void main(String[] args) {
         int[] pegs = {2, 3, 4, 2, 1};
 
+        //heuristic is that we can divide the array into segments wether increasing or decreasing
         int result = RingToss.Greedy(pegs);
 
         System.out.println(result);
